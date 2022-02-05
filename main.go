e1a6e5930969207ee97630dfc9a76e487b53cffb
Adet <adetia.marhadi>
2022-02-05 20:04:37 +0700

    create controller and group it

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/adetiamarhadi/golang-rest-api/config"
+	"github.com/adetiamarhadi/golang-rest-api/controller"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB = config.SetupDatabaseConnection()
+var authController controller.AuthController = controller.NewAuthController()
 
 func main() {
+	defer config.CloseDatabaseConnection(db)
+
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 
 	r.Run()
 }
